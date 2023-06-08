e378fc738b8020f24f15731bed82c6591e7476df
AnnaKim <annakim@inbox.ru>
2023-06-08 14:26:31 +0300

    phonebook

---
+++ src/main/java/Seminar5Java/Phonebook2.java
@@ -0,0 +1,41 @@
+package Seminar5Java;
+import java.util.*;
+public class Phonebook2 {
+    public static void main(String[] args) {
+        Map<String, List<String>> phoneBook = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите количество записей в телефонной книге:");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введите имя и номер телефона через пробел:");
+            String name = scanner.next();
+            String phone = scanner.next();
+            List<String> phones = phoneBook.get(name);
+            if (phones == null) {
+                phones = new ArrayList<>();
+                phones.add(phone);
+                phoneBook.put(name, phones);
+            } else {
+                phones.add(phone);
+            }
+        }
+
+        List<Map.Entry<String, List<String>>> list = new ArrayList<>(phoneBook.entrySet());
+        list.sort(new Comparator<Map.Entry<String, List<String>>>() {
+            @Override
+            public int compare(Map.Entry<String, List<String>> o1, Map.Entry<String, List<String>> o2) {
+                return o2.getValue().size() - o1.getValue().size();
+            }
+        });
+
+        System.out.println("Телефонная книга:");
+        for (Map.Entry<String, List<String>> entry : list) {
+            String name = entry.getKey();
+            List<String> phones = entry.getValue();
+            System.out.println(name + ": " + phones);
+        }
+    }
+
+}
